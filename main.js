ebfba4e9c901dea53caf6a5370f966a2378f233a
Олеся Пантюхина <appw1@mail.ru>
2021-08-18 01:09:29 +0400

    added saving of the drawing

--- main.js
+++ main.js
@@ -9,16 +9,17 @@
 
     let isPainting =  false;
 
+    //рисование
     function drawing(e){
         if (isPainting){
             context.lineWidth = penSize;
             context.strokeStyle = penColor;
             context.lineCap = 'round';
-            context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+            context.lineTo(e.offsetX, e.offsetY);
             context.stroke();
 
             context.beginPath();
-            context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+            context.lineTo(e.offsetX, e.offsetY);
         }
     }
 
@@ -34,18 +35,27 @@
 
     canvas.addEventListener('mousemove', drawing)
 
+    //очистить 
     const clearBtn = document.querySelector('.clear-btn');
     clearBtn.addEventListener('click',()=>{
         context.clearRect(0,0,canvas.width, canvas.height);
     })
 
+    //изменение размера кисти
     const sizeInput = document.querySelector('.pen-size');
     sizeInput.addEventListener('change',()=>{
         penSize = sizeInput.value;
     })
 
+    //измение цвета кисти
     const colorInput = document.querySelector('.pen-color');
     colorInput.addEventListener('change',(e)=>{
         penColor = colorInput.value;
         console.log(penColor);
+    })
+
+    //сохранение рисунка
+    const downloadBtn = document.querySelector(".download-btn");
+    downloadBtn.addEventListener('click',(e)=>{
+        e.target.href = canvas.toDataURL()
     })
