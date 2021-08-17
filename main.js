9d912bbbf416c725c412a3c38a97519b957292c1
Олеся Пантюхина <appw1@mail.ru>
2021-08-18 00:47:08 +0400

    added changing the size and color of the brush

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
     const canvas = document.querySelector('#canvas');
     const context = canvas.getContext('2d');
-
-    const clearBtn = document.querySelector('.clear-btn');
     
-    canvas.height = innerHeight -200;
+    canvas.height = 400;
     canvas.width = 600;
     let penSize = 10;
+    let penColor = 'black';
 
     let isPainting =  false;
 
     function drawing(e){
         if (isPainting){
             context.lineWidth = penSize;
+            context.strokeStyle = penColor;
             context.lineCap = 'round';
             context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
             context.stroke();
@@ -34,6 +34,18 @@
 
     canvas.addEventListener('mousemove', drawing)
 
+    const clearBtn = document.querySelector('.clear-btn');
     clearBtn.addEventListener('click',()=>{
         context.clearRect(0,0,canvas.width, canvas.height);
+    })
+
+    const sizeInput = document.querySelector('.pen-size');
+    sizeInput.addEventListener('change',()=>{
+        penSize = sizeInput.value;
+    })
+
+    const colorInput = document.querySelector('.pen-color');
+    colorInput.addEventListener('change',(e)=>{
+        penColor = colorInput.value;
+        console.log(penColor);
     })
